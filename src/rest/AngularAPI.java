1ddf108f611445cebc789866ea306555970f26b4
Ewerton Campos <ewtn@live.com>
2018-04-22 13:34:28 -0300

    Corrigindo arquivo de configuracao do projeto

--- src/rest/AngularAPI.java
+++ src/rest/AngularAPI.java
@@ -2,21 +2,17 @@ package rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/message")
+@Path("/angularAPI")
 public class AngularAPI 
 {
 
 	@GET
-	@Path("/{param}")
-	public Response printMessage(@PathParam("param") String msg) {
-
-		String result = "Restful example : " + msg;
-
-		return Response.status(200).entity(result).build();
-
+	@Path("/reloadImagem")
+	public Response printMessage() 
+	{
+		return Response.status(200).entity("").build();
 	}
 
 }
